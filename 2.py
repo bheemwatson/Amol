a369959f87b783f3c1e270efd0475122899c1779
James <jamesanderson@gmail.com>
2023-11-23 18:27:12 +0000

    Stash commit

---
+++ 2.py
@@ -0,0 +1,13 @@
+# union of set
+a = {10,20,30,40}
+b = {30,40,50,60}
+print(a|b)
+c = a.union(b)
+print(c)
+
+# intersection of set
+a = {10,20,30,40,50,60}
+b = {40,60,70,50,80,90}
+print(a&b)
+c = a.intersection(b)
+print(c)
